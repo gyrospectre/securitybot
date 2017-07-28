5f2adce3becd74ae78a68fc8b138832d70156086
Antoine Cardon <antoine.cardon@algolia.com>
2017-07-28 11:19:38 +0200

    Makes config a singleton to prevent overrides

--- securitybot/config.py
+++ securitybot/config.py
@@ -2,19 +2,21 @@ __author__ = 'Antoine Cardon'
 __email__ = 'antoine.cardon@algolia.com'
 
 import yaml
-from typing import Dict
 import logging
+from typing import Dict
+from securitybot.utils.class_helper import Singleton
 
 
-class Config:
+class Config(object, metaclass=Singleton):
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self._config = {}
 
     def load_config(self, config_path: str="") -> Dict:
         logging.info('Loading configuration.')
         with open(config_path, 'r') as f:
             self._config = yaml.safe_load(f)
+            self._config['queries'] = {}
             try:
                 self._config['messages'] = yaml.safe_load(
                     open(self._config['includes']['messages_path']))
@@ -23,6 +25,9 @@ class Config:
             except KeyError as e:
                 logging.error('Missing parameter: {0}'.format(e))
                 raise Exception('Configuration file missing parameters.')
+
+        with open(self._config['includes']['queries'][self._config['database']['queries']], 'r') as qfile:
+            self._config['queries'] = yaml.safe_load(qfile)
 
     def get(self, arg, default=None):
         return self._config.get(arg, default)
